6aa17d1bc78d3bd2f318a36ba819329e01a91652
zhouxue <1103542985@qq.com>
2022-04-18 18:17:07 +0800

    更新

--- src/action.js
+++ src/action.js
@@ -1,12 +1,15 @@
 // 此action文件为定义微应用之间全局状态
 // 引入qiankun的应用间通信方法initGlobalState
 import { initGlobalState } from 'qiankun'
-import { useStore } from "vuex";
-const store = useStore()
+// import { useStore } from "vuex";
+import { store } from './store/index'
+// const store = useStore()
 
 const initialState = {
     // 这里可以写初始化数据
-    menu: []
+    menu: [],
+    token: store.state.user.token,
+    userInfo: store.state.user.userInfo
 }
 
 const actions = initGlobalState(initialState)
